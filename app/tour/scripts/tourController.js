c13ae00a68d0317ca4555ed09ce8eba54fa9ac1a
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-09 12:31:30 -0500

    refactored tourController.js

--- app/tour/scripts/tourController.js
+++ app/tour/scripts/tourController.js
@@ -2,10 +2,33 @@ angular
   .module('tour', [])
   .controller('TourController', function($scope) {
 
-    $scope.id = steroids.view.params.id;
-    $scope.tour = {
-        title: "Scavenger hunt",
-        description: "Get ready for a super awesome scavenger hunt!"
-    };
-
+    var index = steroids.view.params.id,
+    	tours = [
+	    	{
+	    		name: 'Red Tour',
+	    		tourClass: 'redtour',
+	    		textClass: 'redtext',
+	    		description: 'The red tour is an ordered tour through the museum!'
+	    	},
+	    	{
+	    		name: 'Blue Tour',
+	    		tourClass: 'bluetour',
+	    		textClass: 'bluetext',
+	    		description: 'The blue tour is an random tour through the museum!'
+	    	},
+	    	{
+	    		name: 'Purple Tour',
+	    		tourClass: 'purpletour',
+	    		textClass: 'purpletext',
+	    		description: 'The purple tour is a scavenger hunt setup in the museum!'
+	    	},
+	    	{
+	    		name: 'Black Tour',
+	    		tourClass: 'blacktour',
+	    		textClass: 'blacktext',
+	    		description: 'The black tour is a tour that takes you to objects often overlooked in the museum!'
+	    	}
+	    ];
+    $scope.tour = tours[index];
+    
   });
