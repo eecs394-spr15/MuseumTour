fcf2b2dc4cbd8e171c88f2370f974d7f66b929e4
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-09 13:11:50 -0500

    dataService.js added, controllers refactored to use the service to ask tours data

--- app/tour/scripts/tourController.js
+++ app/tour/scripts/tourController.js
@@ -1,34 +1,8 @@
 angular
-  .module('tour', [])
-  .controller('TourController', function($scope) {
+  .module('tour', ['common'])
+  .controller('TourController', function($scope, DataService) {
 
-    var index = steroids.view.params.id,
-    	tours = [
-	    	{
-	    		name: 'Red Tour',
-	    		tourClass: 'redtour',
-	    		textClass: 'redtext',
-	    		description: 'The red tour is an ordered tour through the museum!'
-	    	},
-	    	{
-	    		name: 'Blue Tour',
-	    		tourClass: 'bluetour',
-	    		textClass: 'bluetext',
-	    		description: 'The blue tour is an random tour through the museum!'
-	    	},
-	    	{
-	    		name: 'Purple Tour',
-	    		tourClass: 'purpletour',
-	    		textClass: 'purpletext',
-	    		description: 'The purple tour is a scavenger hunt setup in the museum!'
-	    	},
-	    	{
-	    		name: 'Black Tour',
-	    		tourClass: 'blacktour',
-	    		textClass: 'blacktext',
-	    		description: 'The black tour is a tour that takes you to objects often overlooked in the museum!'
-	    	}
-	    ];
-    $scope.tour = tours[index];
+    var index = steroids.view.params.id;
+    $scope.tour = DataService.getTourByIndex(index);
     
   });
--- app/tours-list/scripts/ToursListController.js
+++ app/tours-list/scripts/ToursListController.js
@@ -1,35 +1,10 @@
 angular
-  .module('tours-list', [])
-  .controller('ToursListController', function($scope) {
+  .module('tours-list', ['common'])
+  .controller('ToursListController', function($scope, DataService) {
 
     $scope.app = {
         welcome: "Welcome to the Hull House!"
     };
-    $scope.tours = [
-    	{
-    		name: 'Red Tour',
-    		tourClass: 'redtour',
-    		textClass: 'redtext',
-    		description: 'The red tour is an ordered tour through the museum!'
-    	},
-    	{
-    		name: 'Blue Tour',
-    		tourClass: 'bluetour',
-    		textClass: 'bluetext',
-    		description: 'The blue tour is an random tour through the museum!'
-    	},
-    	{
-    		name: 'Purple Tour',
-    		tourClass: 'purpletour',
-    		textClass: 'purpletext',
-    		description: 'The purple tour is a scavenger hunt setup in the museum!'
-    	},
-    	{
-    		name: 'Black Tour',
-    		tourClass: 'blacktour',
-    		textClass: 'blacktext',
-    		description: 'The black tour is a tour that takes you to objects often overlooked in the museum!'
-    	}
-    ];
+    $scope.tours = DataService.getAllTours();
 
   });
