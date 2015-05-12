818be9dd6ef43b88d8f959c4e0e42d8754e2c222
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-11 21:03:24 -0500

    templating working now, I think

--- app/poi/scripts/poiController.js
+++ app/poi/scripts/poiController.js
@@ -1,9 +1,10 @@
-angular
-  .module('poi')
-  .controller('poiController', function($scope, supersonic, DataService) {
+angular.module('poi', ['common'])
+  .controller('poiController', function($scope, DataService) {
     // Controller functionality here
     var index = steroids.view.params.id;
-    steroids.logger.log(index);
-   	$scope.index = index;
-    $scope.tour = DataService.getTourByIndex(index);
+    var tour = DataService.getTourByIndex(index);
+    $scope.poi = tour.poi[index];
+    steroids.logger.log($scope.poi);
+
+
   });
